c937b1bcb1f6baa169ded9b283cffe9c3bee4d45
DavidKal29 <davidkalmuk65@gmail.com>
2025-09-09 14:27:56 +0200

    Mejoramos los estilos del header para que sean más estéticos y quitamos los colores arcoiris

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,28 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
 
-export default function Header({ currentUser, parametros = {} }) {
-  const [menuOpen, setMenuOpen] = useState(false);
-
+export default function Header() {
   return (
-    <header className="bg-gradient-to-r from-gray-800 via-gray-900 to-black z-50 flex justify-between items-center py-[10px] px-[15px] sm:px-[35px] fixed top-0 w-full h-[80px] md:h-[100px]">
+    <header className="bg-gradient-to-r from-gray-800 via-gray-900 to-black shadow-2xl z-50 flex justify-between items-center py-3 px-6 sm:px-12 fixed top-0 w-full h-[80px] md:h-[100px]">
+
       {/* Logo */}
-      <Link to="/" className="w-[16%] min-[568px]:w-[10%] sm:w-[10%] lg:w-[7%] xl:w-[5%]">
+      <Link to="/" className="w-[16%] min-[568px]:w-[12%] sm:w-[10%] lg:w-[8%] xl:w-[6%]">
         <img className="w-full" src="./logo.png" alt="Logo" />
       </Link>
 
-
       {/* Botones */}
-      <div className="flex items-center">
-        <div id="botones" className="flex gap-4">
-            <Link to="#">
-              <button className="bg-gradient-to-r from-blue-500 to-pink-500 text-white rounded px-[16px] py-[8px]  font-bold text-[14px] cursor-pointer">
-                Iniciar Sesión
-              </button>
-            </Link>
-            <Link to="#">
-              <button className="bg-gradient-to-r from-blue-500 to-orange-500 text-white rounded px-[16px] py-[8px]  font-bold text-[14px] cursor-pointer">
-                Crear cuenta
-              </button>
-            </Link>
-          </div>
+      <div className="flex items-center gap-4 md:gap-6">
+        <Link to="/login">
+          <button className="bg-gradient-to-r from-blue-700 to-blue-500 shadow-lg text-white rounded-full px-5 py-2 font-semibold text-sm md:text-base cursor-pointer">
+            Iniciar Sesión
+          </button>
+        </Link>
+        <Link to="/register">
+          <button className="bg-gradient-to-r from-purple-700 to-purple-500 shadow-lg text-white rounded-full px-5 py-2 font-semibold text-sm md:text-base cursor-pointer">
+            Crear cuenta
+          </button>
+        </Link>
       </div>
 
     </header>
   );
 }
-
